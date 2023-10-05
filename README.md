39cfb6b5f4f857a505dea5679f44994c25a309cb
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2023-10-05 09:54:27 -0300

    Added text to readme.md

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test-commitCommiter bot by Maraba23 
+Committer bot by Maraba23
--- commiter.py
+++ commiter.py
@@ -2,47 +2,49 @@ import os
 import sys
 import time
 
-def main():
+def add_text_to_readme():
+    with open("readme.md", "a") as file:
+        file.write("Committer bot by Maraba23\n")
 
+def delete_last_line_from_readme():
+    with open("readme.md", "r") as file:
+        lines = file.readlines()
+    with open("readme.md", "w") as file:
+        file.writelines(lines[:-1])
+
+def main():
     commits = int(input("How many commits do you want to make? "))
 
-    #check readme.md
+    # Check readme.md
     if not os.path.exists("readme.md"):
         print("readme.md not found")
         return
-    #check if git is installed
+
+    # Check if git is installed
     if os.system("git --version") != 0:
         print("git not found")
         return
-    '''
-    Add this text to readme.md
 
-    Commiter bot by Maraba23
-    '''
+    add_text_to_readme()
 
-    #add text to readme.md
-    os.system("echo Commiter bot by Maraba23 >> readme.md")
-    #commit and push
+    # Commit and push
     os.system("git add .")
     os.system("git commit -m \"Added text to readme.md\"")
     os.system("git push")
 
-    #loop (delete the text, then add it again)
     for i in range(commits):
-        #delete text
-        os.system("sed -i '$ d' readme.md")
-        #commit and push
+        delete_last_line_from_readme()
+
+        # Commit and push
         os.system("git add .")
         os.system("git commit -m \"Deleted text from readme.md\"")
-        os.system("git push -u origin master")
-        #wait 30 seconds
+        os.system("git push")
         time.sleep(30)
-        #add text
-        os.system("echo Commiter bot by Maraba23 >> readme.md")
-        #commit and push
+
+        add_text_to_readme()
         os.system("git add .")
         os.system("git commit -m \"Added text to readme.md\"")
-        os.system("git push -u origin master")
+        os.system("git push")
 
 if __name__ == "__main__":
     main()
